db1069d19357517eaa0c629ce4973b1e0f3e86ff
mvlombardo <mvlombardo@gmail.com>
2020-07-08 14:58:56 +0200

    updated README

--- GCLUST/_run_gclust_parcellation.sh
+++ GCLUST/_run_gclust_parcellation.sh
@@ -2,15 +2,16 @@
 
 # run gclust_sub.sh over subjects
 
-codedir=/home/ml437/code
-cd $codedir
-
 # load relevant modules for doing stuff with Freesurfer and MATLAB
 module load freesurfer/6.0.0
 module load matlab/r2019b
 
 # directory where fsaverage is located
 fsavgdir=$FREESURFER_HOME/subjects/fsaverage
+
+# directories - may need to change based on where your files are...
+codedir=/home/ml437/code
+cd $codedir
 
 # set the rootdir where all the UK Biobank Imaging data will be kept
 rootdir=/rds/project/rb643/rds-rb643-ukbiobank2/Data_Imaging
@@ -36,7 +37,6 @@ mkdir $final_out_sa_dir
 final_out_ct_dir=$rootoutdir/cortical_thickness
 mkdir $final_out_ct_dir
 
-
 # get a list of subjects to loop over
 cd $rootdir
 sublist=`ls -d UKB*`
@@ -49,5 +49,5 @@ do
 		--partition=skylake-himem \
 		--output=${outdir}/logs/${sub}_fslog.log \
 		--nodes=1 --ntasks=1 --cpus-per-task=1 --time=20:00:00 --mem=12000 \
-		$codedir/gclust_sub.sh ${sub} 
+		$codedir/gclust_sub.sh ${sub}
 done # for sub in $sublist
