ad19c0577ddee84320c37ad9cc0a7031cc6d5d52
mvlombardo <mvlombardo@gmail.com>
2020-07-08 09:54:01 +0200

    modified gclust.sh

--- GCLUST/gclust.sh
+++ GCLUST/gclust.sh
@@ -11,32 +11,28 @@ cd $codedir
 module load freesurfer/6.0.0
 module load matlab/r2019b
 
-# this was in gclust.csh but not sure if I need it.
-# source $FREESURFER_HOME/SetUpFreeSurfer.sh # edit as necessary
-
-
 # directory where fsaverage is located
 fsavgdir=$FREESURFER_HOME/subjects/fsaverage
 
 # set the rootdir where all the UK Biobank Imaging data will be kept
 rootdir=/rds/project/rb643/rds-rb643-ukbiobank2/Data_Imaging
 
-# GCLUST GitHub repo directories
-gclustdir=/home/ml437/code/GCLUST
-matlabdir=$gclustdir/GCLUST/matlab
-roidir=$gclustdir/GCLUST/clusters
-
-# set the output directory where data will be stored after all this is finished running
-rootoutdir=/home/ml437/rds/hpc-work/ukbiobank
-export SUBJECTS_DIR=$rootoutdir
-#datadir=$rootoutdir/surfdata
-#mkdir $datadir
-mkdir $rootoutdir/GCLUST
-
 # get a list of subjects to loop over
 cd $rootdir
-sublist=`ls -d UKB10000*`
+sublist=`ls -d UKB10000* UKB10001*`
 cd $codedir
+
+# set the output directory where data will be stored after all this is finished running
+rootoutdir=/rds/user/ml437/hpc-work/ukbiobank/GCLUST2
+export SUBJECTS_DIR=$rootoutdir
+mkdir $rootoutdir
+outdir=$rootoutdir
+ln -s $fsavgdir $outdir
+matlabdir=$outdir/matlab
+cp -Rf $codedir/GCLUST/GCLUST/matlab $matlabdir
+roidir=$codedir/GCLUST/GCLUST/clusters
+datadir=$outdir/surfdata
+mkdir $datadir
 
 # set some variables
 nsmooth=705
@@ -45,71 +41,66 @@ meas="thickness"
 volume="volume"
 target="fsaverage"
 
-# loop over subjects
+fname_subjlist=$datadir/subjlist.txt
+# make subjlist.txt and symbolic links
 for sub in $sublist
 do
 
     # print progress to screen
     echo Working on $sub
-
-    # make a symbolic link from $rootdir/$sub in $rootoutdir
-    ln -s $rootdir/$sub $rootoutdir
     
-    # make a GCLUST directory in $rootoutdir to place results into
-    #mkdir $rootoutdir/$sub
-    # make subject specific directory in GCLUST output directory
-    suboutdir=$rootoutdir/GCLUST/$sub
-    mkdir $suboutdir
-    ln -s $fsavgdir $rootoutdir
-    cd $rootoutdir
+    # make subjlist.txt 
+    echo $sub >> $datadir/subjlist.txt
 
-    # left hemisphere ---------------------------------------------------------
-    hemi="lh"
+    # make directories 
+    subindir=$outdir/$sub
+    mkdir $subindir
 
-    # set subject's *sphere.reg file
-    subj_filename=$rootdir/$sub/surfaces/$sub/surf/${hemi}.sphere.reg
-
-    # resample thickness to common space and concatenate 
-    out=$suboutdir/${hemi}.${meas}
-    mris_preproc --out ${out}.mgh --target $target --f $subj_filename --hemi $hemi --meas $meas --s $sub
-
-    # smooth thickness
-    mri_surf2surf --s $target --sval ${out}.mgh --cortex --nsmooth-out $nsmooth --tval ${out}.n${nsmooth}.mgh --hemi $hemi 
-
-    # resample surface area to common space and concatenate 
-    out=$suboutdir/${hemi}.${area}
-    mris_preproc --out ${out}.mgh --target $target --f $subj_filename --hemi $hemi --area $area --s $sub
-
-    # smooth surface
-    mri_surf2surf --s $target --sval ${out}.mgh --cortex --nsmooth-out $nsmooth --tval ${out}.n${nsmooth}.mgh --hemi $hemi
-
-
-    # right hemisphere --------------------------------------------------------
-    hemi="rh"
-
-    # set subject's *sphere.reg file
-    subj_filename=$rootoutdir/$sub/surfaces/$sub/surf/${hemi}.sphere.reg
-
-    # resample thickness to common space and concatenate 
-    out=$suboutdir/${hemi}.${meas}
-    mris_preproc --out ${out}.mgh --target $target --f $subj_filename --hemi $hemi --meas $meas --s $sub
-
-    # smooth thickness
-    mri_surf2surf --s $target --sval ${out}.mgh --cortex --nsmooth-out $nsmooth --tval ${out}.n${nsmooth}.mgh --hemi $hemi
-
-    # resample surface area to common space and concatenate 
-    out=$suboutdir/${hemi}.${area}
-    mris_preproc --out ${out}.mgh --target $target --f $subj_filename --hemi $hemi --area $area --s $sub
-
-    # smooth surface
-    mri_surf2surf --s $target --sval ${out}.mgh --cortex --nsmooth-out $nsmooth --tval ${out}.n${nsmooth}.mgh --hemi $hemi
+    # make a symbolic link to raw data
+    rawdatadir=$rootdir/$sub/surfaces/$sub
+    ln -s $rawdatadir/* $subindir
 
 done
 
+# left hemisphere ---------------------------------------------------------
+hemi="lh"
+
+# resample thickness to common space and concatenate 
+out=${datadir}/${hemi}.${meas}
+mris_preproc --out ${out}.mgh --target ${target} --f $fname_subjlist --hemi $hemi --meas ${meas}
+
+# smooth thickness
+mri_surf2surf --s $target --sval ${out}.mgh --cortex --nsmooth-out $nsmooth --tval ${out}.n${nsmooth}.mgh --hemi $hemi
+
+# resample surface area to common space and concatenate 
+out=${datadir}/${hemi}.${area}
+mris_preproc --out ${out}.mgh --target ${target} --f $fname_subjlist --hemi $hemi --area $area
+
+# smooth surface
+mri_surf2surf --s $target --sval ${out}.mgh --cortex --nsmooth-out $nsmooth --tval ${out}.n${nsmooth}.mgh --hemi $hemi
+
+
+# right hemisphere ---------------------------------------------------------
+hemi="rh"
+
+# resample thickness to common space and concatenate 
+out=${datadir}/${hemi}.${meas}
+mris_preproc --out ${out}.mgh --target ${target} --f $fname_subjlist --hemi $hemi --meas ${meas}
+
+# smooth thickness
+mri_surf2surf --s $target --sval ${out}.mgh --cortex --nsmooth-out $nsmooth --tval ${out}.n${nsmooth}.mgh --hemi $hemi
+
+# resample surface area to common space and concatenate 
+out=${datadir}/${hemi}.${area}
+mris_preproc --out ${out}.mgh --target ${target} --f $fname_subjlist --hemi $hemi --area $area
+
+# smooth surface
+mri_surf2surf --s $target --sval ${out}.mgh --cortex --nsmooth-out $nsmooth --tval ${out}.n${nsmooth}.mgh --hemi $hemi
+
+cd $codedir/GCLUST/GCLUST
+
+# extract weighted averages for area and thickness for each cluster
+cmd="gclust('"$datadir"','"$roidir"','"$matlabdir"','"$outdir"')"
+matlab -nosplash -nojvm -r "try, $cmd; exit; catch e, fprintf('ERROR in matlab: %s\n',e.message); exit; end;"
+
 cd $codedir
-
-# # extract weighted averages for area and thickness for each cluster
-# set cmd = "gclust('"$datadir"','"$roidir"','"$matlabdir"','"$outdir"')"
-# matlab -nosplash -nojvm -r \
-#  "try, $cmd; exit; catch e, fprintf('ERROR in matlab: %s\n',e.message); exit; end;"
-
